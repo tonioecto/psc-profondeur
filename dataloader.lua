874a59b8ddca86743e910660283bc61e3a4fdf55
shiyicn <shiyi.cn11@gmail.com>
2017-01-25 23:12:36 +0100

    change dataloader by jianfei

--- dataloader.lua
+++ dataloader.lua
@@ -15,18 +15,140 @@ function M.load()
     return trainSet, validationSet
 end
 
+
 function DataLoader:__init(imageset, depthset,opt)
    self.imageset =imageset
    self.depthset = depthset
    --self.indexstart = indexstart
-   self.size = opt.samplesize
+   self.size = opt.sampleSize
    self.batchsize = opt.batchSize
 end
 
-function DataLoader:loadDataset()       --Load the images and depthMap, and generate dataset for trainning
 
-    print('The self.sizeber of image is:'..#self.imagename)
-    print('The self.sizeber of correponding depthmap is:'..#self.depthname)
+
+function DataLoader:creDatatable()
+    self.imagename = {}
+    self.depthname = {}
+
+    self.trainImageTable = {}
+    self.trainDepthTable = {}
+
+    self.valImageTable = {}
+    self.valDepthTable = {}
+
+    self.testImageTable = {}
+    self.testDepthTable = {}
+
+    for file in paths.files(self.imageset) do
+        if file:find(".*(jpg)$") then
+            table.insert(self.imagename, paths.concat(self.imageset,file))
+        end
+    end
+
+    if #self.imagename == 0 then
+        error('given directory doesn\'t contain any JPG files')
+    end
+
+    table.sort(self.imagename, function (a,b) return a < b end)
+    --print(#self.imagename)
+
+    local indexFakefile = {}
+    for i,file in ipairs(self.imagename) do
+        local name = file:match(".+/img(.*).jpg$")
+        local index = 'depth'..name
+        local fullname = index..'.mat'
+        local matname = paths.concat(self.depthset,fullname)
+        if paths.filep(matname)then
+            table.insert(self.depthname,matname)
+        else
+            table.insert(indexFakefile,i)
+        end
+    end
+    for i=#indexFakefile,1,-1 do
+        table.remove(self.imagename,indexFakefile[i])
+    end
+
+    print('The number of image is:'..#self.imagename)
+    print('The number of correponding depthmap is:'..#self.depthname)
+
+    if #self.imagename == 0 then
+        error('given directory doesn\'t contain any JPG files')
+    end
+
+    --create table of trainset,validationset and testset
+    self:tableShuffle("all")    --shuffle the table firstly
+
+    local dataSetSize = #self.imagename
+
+    local trainSetSize = 0.8 * dataSetSize
+    local valiSetSize = 0.9 * dataSetSize
+
+    for i=1,trainSetSize,1 do
+      table.insert(self.trainImageTable,self.imagename[i])
+      table.insert(self.trainDepthTable,self.depthname[i])
+    end
+
+    for i = trainSetSize+1,valiSetSize,1 do
+      table.insert(self.valImageTable,self.imagename[i])
+      table.insert(self.valDepthTable,self.depthname[i])
+    end
+
+    for i = valiSetSize+1,dataSetSize,1 do
+      table.insert(self.testImageTable,self.imagename[i])
+      table.insert(self.testDepthTable,self.depthname[i])
+    end
+    --return self.imagename,self.depthname
+end
+
+
+function DataLoader:loadDataset(s)       --Load the images and depthMap, and generate dataset for trainning
+
+  --[[
+    local self.imagename = {}
+    local self.depthname = {}
+
+    for file in paths.files(imageSet) do
+        if file:find(".*(jpg)$") then
+            table.insert(self.imagename, paths.concat(imageSet,file))
+        end
+    end
+
+    if #self.imagename == 0 then
+        error('given directory doesn\'t contain any JPG files')
+    end
+
+    table.sort(self.imagename, function (a,b) return a < b end)
+    print(#self.imagename)
+
+    local indexFakefile = {}
+    for i,file in ipairs(self.imagename) do
+        local name = file:match(".+/img(.*).jpg$")
+        local index = 'depth'..name
+        local fullname = index..'.mat'
+        local matname = paths.concat(depthSet,fullname)
+        if paths.filep(matname)then
+            table.insert(self.depthname,matname)
+        else
+            table.insert(indexFakefile,i)
+        end
+    end
+    for i=#indexFakefile,1,-1 do
+        table.remove(self.imagename,indexFakefile[i])
+    end
+   ]]
+    local imagetable = {}
+    local depthtable = {}
+    if s=="val" then
+      print('loading validation dataset')
+      imagetable = self.valImageTable
+      depthtable = self.valDepthTable
+    elseif s=="test" then
+      print('loading test dataset')
+      imagetable = self.testImageTable
+      depthtable = self.testDepthTable
+    end
+    print('The number of image is:'..#imagetable)
+    print('The number of correponding depthmap is:'..#depthtable)
 
     if #self.imagename == 0 then
         error('given directory doesn\'t contain any JPG files')
@@ -65,26 +187,27 @@ function DataLoader:loadDataset()       --Load the images and depthMap, and gene
 
 end
 
-function DataLoader:miniBatchload(dataset)   --create mini batch 
+
+function DataLoader:miniBatchload(dataset)   --create mini batch
     --randomize the data firstly
-    local shuffle = torch.randperm(dataset:size())
+    --local shuffle = torch.randperm(dataset:size())
     local imageSize = dataset.image[1]:size()
     local depthSize = dataset.depth[1]:size()
 
-    local self.sizeBatch = math.floor(dataset:size()/self.batchsize)
-    --print(self.sizeBatch)
-    local imageSet = torch.Tensor(self.sizeBatch,self.batchsize,unpack(imageSize:totable()))
-    local depthSet = torch.Tensor(self.sizeBatch,self.batchsize,unpack(depthSize:totable()))
+    local numBatch = math.floor(dataset:size()/self.batchsize)
+    --print(numBatch)
+    local imageSet = torch.Tensor(numBatch,self.batchsize,unpack(imageSize:totable()))
+    local depthSet = torch.Tensor(numBatch,self.batchsize,unpack(depthSize:totable()))
 
-    for index = 1,self.sizeBatch,1
+    for index = 1,numBatch,1
     do
-        local self.sizeRemain = dataset:size() - (index-1)*self.batchsize
-        if(self.sizeRemain >=self.batchsize)then
+        local numRemain = dataset:size() - (index-1)*self.batchsize
+        if(numRemain >=self.batchsize)then
             local indexBegin = (index-1)*self.batchsize + 1
             local indexEnd = indexBegin + self.batchsize - 1
             for k = 1,self.batchsize,1 do
-                imageSet[{index,k,{}}] = dataset.image[shuffle[indexBegin]]
-                depthSet[{index,k,{}}] = dataset.depth[shuffle[indexBegin]]
+                imageSet[{index,k,{}}] = dataset.image[indexBegin]
+                depthSet[{index,k,{}}] = dataset.depth[indexBegin]
                 indexBegin = indexBegin + 1
             end
         end
@@ -105,67 +228,42 @@ function DataLoader:miniBatchload(dataset)   --create mini batch
     return data
 end
 
-function DataLoader:creDatatable()
-    self.imagename = {}
-    self.depthname = {}
 
-    for file in paths.files(self.imageset) do
-        if file:find(".*(jpg)$") then
-            table.insert(self.imagename, paths.concat(self.imageset,file))
-        end
+
+function DataLoader:tableShuffle(s)
+  local rand = math.random
+  if s=='all' then
+    local iteration = #self.imagename
+    local j
+
+    for i=iteration,2,-1 do
+      j = rand(i)
+      self.imagename[i],self.imagename[j] = self.imagename[j],self.imagename[i]
+      self.depthname[i],self.depthname[j] = self.depthname[j],self.depthname[i]
+    end
+  elseif s=='train' then
+    local iteration = #self.trainImageTable
+    local j
+
+    for i=iteration,2,-1 do
+      j = rand(i)
+      self.trainImageTable[i],self.trainImageTable[j] = self.trainImageTable[j],self.trainImageTable[i]
+      self.trainDepthTable[i],self.trainDepthTable[j] = self.trainDepthTable[j],self.trainDepthTable[i]
     end
 
-    if #self.imagename == 0 then
-        error('given directory doesn\'t contain any JPG files')
-    end
-
-    table.sort(self.imagename, function (a,b) return a < b end)
-    print(#self.imagename)
-
-    local indexFakefile = {}
-    for i,file in ipairs(self.imagename) do
-        local name = file:match(".+/img(.*).jpg$")
-        local index = 'depth'..name
-        local fullname = index..'.mat'
-        local matname = paths.concat(self.depthset,fullname)
-        if paths.filep(matname)then
-            table.insert(self.depthname,matname)
-        else
-            table.insert(indexFakefile,i)
-        end
-    end
-    for i=#indexFakefile,1,-1 do
-        table.remove(self.imagename,indexFakefile[i])
-    end
-
-    print('The self.sizeber of image is:'..#self.imagename)
-    print('The self.sizeber of correponding depthmap is:'..#self.depthname)
-
-    if #self.imagename == 0 then
-        error('given directory doesn\'t contain any JPG files')
-    end
-
-    return self.imagename,self.depthname
+  end
 end
 
-function DataLoader:tableShuffle()
-    local shuffle = torch.randperm(#self.imagename)
-    local image = {}
-    local depth = {}
-    for index = 1,#self.imagename,1 do
-        image[index] = self.imagename[shuffle[index]]
-        depth[index] = self.depthname[shuffle[index]]
-    end
-    return image,depth
-end
+
 
 function DataLoader:loadDatafromtable(indexstart)
-    if #self.imagename == 0 then
-        error('given directory doesn\'t contain any JPG files')
+    if #self.trainImageTable == 0 then
+        error('trainSet doesn\'t contain any JPG files')
     end
 
-    local imageRemain = #self.imagename - indexstart + 1
+    local imageRemain = #self.trainImageTable - indexstart + 1
     local sampleRealsize = self.size
+    print(imageRemain)
     if imageRemain < self.size then
         sampleRealsize = imageRemain
     end
@@ -175,10 +273,11 @@ function DataLoader:loadDatafromtable(indexstart)
 
     for i = 1,sampleRealsize,1 do
         local index = indexstart + i - 1
-        local m1 = image.loadJPG(self.imagename[index])
-        local m2 = mat.load(self.depthname[index],'depthMap')
+        local m1 = image.loadJPG(self.trainImageTable[index])
+        local m2 = mat.load(self.trainDepthTable[index],'depthMap')
         m1 = image.scale(m1,304,228,'bicubic')
         m2 = image.scale(m2,128,160,'bicubic')
+
         imageSet[i] = m1
         depthSet[i] = m2
         --indexstart = indexstart + 1
