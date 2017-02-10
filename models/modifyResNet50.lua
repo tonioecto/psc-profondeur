d621831c27d06865014b7fee0764499e555801ab
shiyicn <shiyi.cn11@gmail.com>
2017-02-11 00:09:19 +0100

    small change

--- models/modifyResNet50.lua
+++ models/modifyResNet50.lua
@@ -15,27 +15,27 @@ model:replace(function(module)
 	 if num == 0  then
 		return nn.Identity()
 	 end
-	end   
+	end
 end)
 
 -- delete modules: 'cudnn.ReLU', 'cudnn.SpatialAveragePooling',
 -- 'nn.Linear' and 'nn.View'
 model:replace(function(module)
-	if torch.typename(module)== 'cudnn.ReLU' then
-	 num = num - 1
-	 if num == 0  then
-		return nn.Identity()
-	 end
-	end
-   if torch.typename(module) == 'cudnn.SpatialAveragePooling' then
-      return nn.Identity()
-   elseif torch.typename(module) == 'nn.Linear' then
-	  return nn.Identity()
-   elseif torch.typename(module) == 'nn.View' then
-	  return nn.Identity() 
-   else
-      return module
-   end
+    if torch.typename(module)== 'cudnn.ReLU' then
+        num = num - 1
+        if num == 0  then
+            return nn.Identity()
+        end
+    end
+    if torch.typename(module) == 'cudnn.SpatialAveragePooling' then
+        return nn.Identity()
+    elseif torch.typename(module) == 'nn.Linear' then
+        return nn.Identity()
+    elseif torch.typename(module) == 'nn.View' then
+        return nn.Identity() 
+    else
+        return module
+    end
 end)
 
 -- save the changed model
