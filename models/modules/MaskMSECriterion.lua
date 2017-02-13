307519f5d1145aabbe7410cd152fb2e7deabd6ae
shiyicn <shiyi.cn11@gmail.com>
2017-02-14 00:53:42 +0100

    transforms added

--- models/modules/MaskMSECriterion.lua
+++ models/modules/MaskMSECriterion.lua
@@ -13,11 +13,8 @@ end
 
 function MaskMSECriterion:updateOutput(input, target)
     self.m = self:mask(target)
-    print(input)
     input:maskedFill(self.m, 0)
-    print(input)
     target:maskedFill(self.m, 0)
-    print(target)
     self.output_tensor = self.output_tensor or input.new(1)
     input.THNN.MSECriterion_updateOutput(
     input:cdata(),
@@ -31,8 +28,8 @@ end
 
 function MaskMSECriterion:updateGradInput(input, target)
     self.m = self:mask(target)
-    input = input:maskedFill(self.m, 0)
-    target = target:maskedFill(self.m, 0)
+    input:maskedFill(self.m, 0)
+    target:maskedFill(self.m, 0)
     input.THNN.MSECriterion_updateGradInput(
     input:cdata(),
     target:cdata(),
@@ -45,6 +42,5 @@ end
 function MaskMSECriterion:mask(target)
     local g = torch.ge(target, self.highMask)
     local l = torch.eq(target, self.lowMask)
-    -- print(l + g)
     return l + g
 end
