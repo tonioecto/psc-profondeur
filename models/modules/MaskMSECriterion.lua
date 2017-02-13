e01018f25280483b46b89a6a23e4dd978ecd080f
shiyicn <shiyi.cn11@gmail.com>
2017-02-14 00:45:42 +0100

    transforms added

--- models/modules/MaskMSECriterion.lua
+++ models/modules/MaskMSECriterion.lua
@@ -16,7 +16,7 @@ function MaskMSECriterion:updateOutput(input, target)
     self.m = self:mask(target)
     input = input:maskedFill(self.m, 0)
     target = target:maskedFill(self.m, 0)
-    self.output_tensor = self.output_tensor or input.new(1)
+    self.output_tensor = self.output_tensor
     input.THNN.MSECriterion_updateOutput(
     input:cdata(),
     target:cdata(),
