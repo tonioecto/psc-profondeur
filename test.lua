1515f58ff4b65d6d67b62d9d689ddc61da523520
Jefzhang <jfzhang1993@gmail.com>
2017-03-08 16:11:22 +0100

    add predict test

--- test.lua
+++ test.lua
@@ -66,7 +66,7 @@ for epoch = opt.epochNumber, opt.nEpochs+opt.epochNumber, 1 do
     local depth = pair.depth:cuda()
 
     -- Run model on validation set
-    trainer:predict(epoch, img, depth, dataloder)
+    trainer:predict(epoch, img, depth, dataloader)
 
     local bestModel = false
 
