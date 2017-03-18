f3195faee8d0236a816a0eda929b902033534190
shiyicn <shiyi.cn11@gmail.com>
2017-03-18 14:33:20 +0100

    change val set size to debug code

--- train.lua
+++ train.lua
@@ -218,7 +218,7 @@ function Trainer:predict(epoch, img, depth, dataloader)
 end
 
 function plot(n, loss, frequence, lossTrace, toShow)
-    lossTrace[n/fequence] = loss / frequence
+    lossTrace[n/frequence] = loss / frequence
     if toShow then
         gnuplot.plot(lossTrace)
     end
