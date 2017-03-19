af1481b98e53fd3a49882a06ae449f283f722b7f
shiyicn <shiyi.cn11@gmail.com>
2017-03-19 11:27:46 +0100

    test learning rate recipe

--- opts.lua
+++ opts.lua
@@ -59,9 +59,9 @@ function M.parse(arg)
     end
 
     if opt.dataset == 'make3d' then
-        opt.LR = 0.01
+        opt.LR = 0.05
     elseif opt.dataset == 'nyu' then
-        opt.LR = 0.01
+        opt.LR = 0.05
     else
         cmd:error('unknown dataset: '..opt.dataset)
     end
