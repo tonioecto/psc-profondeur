669411e14e730e52f1ffb6cfffea3468ffaf8890
Jefzhang <jfzhang1993@gmail.com>
2017-04-18 09:03:04 +0200

    update depth show

--- depthMapPlot.py
+++ depthMapPlot.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import torchfile
 
 #depth = np.random.random((128,64));
@@ -18,12 +19,22 @@ print(image.shape);
 pred = np.reshape(result['pred'],(128,96));
 groundTruth = result['groundTruth'];
 
-f, (ax1,ax2) = plt.subplots(1, 2, sharey=True)
+#f, (ax1,ax2) = plt.subplots(1, 2,sharey=True)
 
-ax1.imshow(groundTruth,cmap='gist_rainbow',interpolation='none');
-ax2.imshow(pred,cmap='gist_rainbow',interpolation='none');
+#ax1.imshow(groundTruth,cmap='rainbow',interpolation='none');
+#ax2.imshow(pred,cmap='rainbow',interpolation='none');
+#ax3.imshow(image);
 
-plt.figure();
-plt.imshow(image);
+#plt.figure();
+#plt.imshow(image);
+gs = gridspec.GridSpec(1, 3,
+                       width_ratios=[1,1,1]
+                       );
+ax1 = plt.subplot(gs[0]);
+ax2 = plt.subplot(gs[1]);
+ax3 = plt.subplot(gs[2]);
+ax2.imshow(groundTruth,cmap='rainbow',interpolation='none');
+ax3.imshow(pred,cmap='rainbow',interpolation='none');
+ax1.imshow(image);
 plt.show();
 #print(result[b'image'].shape);
