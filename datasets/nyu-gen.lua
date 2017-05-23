d30941557334f08411804c0570d0ae6a80bf3a2b
shiyicn <shiyi.cn11@gmail.com>
2017-05-23 17:25:02 +0200

    add nyu

---
+++ datasets/nyu-gen.lua
@@ -0,0 +1,155 @@
+require 'paths'
+require 'ffi'
+local T = require 'datasets/transforms'
+local unpack = unpack or table.unpack
+
+local M = {}
+
+local function findImageDepthMatches(imageDir, depthDir)
+
+    local dirsImage = paths.dir(imageDir)
+    table.sort(dirsImage)
+    local imagePath = {}
+    local depthPath = {}
+
+    -- Generate a list of all the images and their depths
+    for _,img in ipairs(dirsImage) do
+        if not (img:find('.jpg') == nil) then
+            local basename = img:match('image(.*).jpg$')
+            -- create possible corresponding depth name
+            local depth = {
+                'image'..basename..'.t7',
+                'depth'..basename..'.t7',
+            }
+            for _,d in ipairs(depth) do
+                if paths.filep(paths.concat(depthDir, d)) then
+                    table.insert(imagePath, paths.concat(imageDir, img))
+                    table.insert(depthPath, paths.concat(depthDir, d))
+                    break
+                end
+            end
+        end
+    end
+
+    print('=> Generate image path file and depth path file with size '
+    ..#imagePath..' and '..#depthPath)
+
+    return imagePath, depthPath
+end
+
+-- generate info file for val and train dataset
+function M.exec(opt, cacheFile, split)
+    -- find the image and depth matches
+    if split ~= 'train' and (split ~= 'val' and split ~= 'test') then
+        error('not a valid split label: '..split)
+    end
+
+    local imageDir = paths.concat(opt.data, split, 'image')
+    local depthDir = paths.concat(opt.data, split, 'depth')
+
+    assert(paths.dirp(imageDir), split..' image directory not found: ' .. imageDir)
+    assert(paths.dirp(depthDir), split..'depth directory not found: ' .. depthDir)
+
+    print("=> Generating list of images and corresponding depths for "..split..' set')
+    local imageDir, depthDir = findImageDepthMatches(imageDir, depthDir)
+
+    local info = {
+        basedir = paths.concat(opt.data, split),
+        imagePath = imageDir,
+        depthPath = depthDir,
+    }
+
+    print(" | saving list of images and depths to " .. cacheFile)
+    torch.save(cacheFile, info)
+
+    return info
+end
+
+-- data augementation process for all inputs in the original directory
+function M.augmentation(imageDirOrigin, depthDirOrigin, opt, split, trainDataPortion, trans)
+
+    -- whole data augmentation for all orginal path images
+    local imagePath = paths.concat(opt.data, split, 'image')
+    local depthPath = paths.concat(opt.data, split, 'depth')
+    paths.mkdir(imagePath)
+    paths.mkdir(depthPath)
+
+    -- find all origin image and depth matches
+    imageDirOrigin = paths.concat(opt.data, imageDirOrigin)
+    depthDirOrigin = paths.concat(opt.data, depthDirOrigin)
+    local imagePathOrigin, depthPathOrigin = findImageDepthMatches(
+    imageDirOrigin, depthDirOrigin
+    )
+    local sizeOrigin = #imagePathOrigin
+
+    local startIndex
+    local endIndex
+    local size
+
+    if split == 'train' then
+        startIndex = 1
+        endIndex = torch.round(sizeOrigin * trainDataPortion)
+    end
+    if split == 'val' then
+        startIndex = torch.round(sizeOrigin * trainDataPortion) + 1
+        endIndex = sizeOrigin
+    end
+    if split == 'test' then
+        startIndex = 1
+        endIndex = sizeOrigin
+    end
+
+    size = endIndex - startIndex + 1
+    local targetSize =size * opt.incre
+
+    -- firstly we scale depth image
+    -- number of data to generate for  each origin image
+    local num = opt.incre
+    print('=> original size of '..split..' '..size)
+    print('=> after augmentation '..split..' '..targetSize)
+
+    -- data augmentation compose
+    --local imageScale = T.Scale(640, 480)
+    -- image scale function rotates width and heigth
+    --local depthScale = T.Scale(640, 480)
+
+    for i = startIndex, endIndex, 1 do
+        -- size of tensor of the load image: 3x640x480
+        local img = image.loadJPG(imagePathOrigin[i])
+        --img = imageScale(img)
+        
+        -- size of tensor of the depth map: 
+        local depth = torch.load(depthPathOrigin[i])
+        --depth = depth:select(3, 4)
+        --depth = depthScale(depth)
+
+        local basename = paths.basename(imagePathOrigin[i])
+        basename = basename:match('image(.*).jpg$')
+
+        local tmpImg
+        local tmpDep
+
+        print('=> process image: '..basename)
+
+        for j = 1, num, 1 do
+            tmpImg, tmpDep = trans(img, depth)
+            image.save(paths.concat(imagePath, 'img'..basename..'-'..j..'.jpg'), tmpImg)
+            torch.save(paths.concat(depthPath, 'depth'..basename..'-'..j..'.t7'), tmpDep)
+        end
+    end
+
+end
+
+function M.augOneMatch(trans)
+
+    return function(img, depth)
+        local imageScale = T.Scale(345, 460)
+        local depthScale = T.Scale(192, 256)
+        img = imageScale(img)
+        depth = depthScale(depth)
+        return trans(img, depth)
+    end
+    
+end
+
+return M
---
+++ datasets/nyu.lua
@@ -0,0 +1,70 @@
+local image = require 'image'
+local paths = require 'paths'
+local T = require 'datasets/transforms'
+local G = require 'datasets/make3d-gen'
+local ffi = require 'ffi'
+
+local M = {}
+local NyuDataset = torch.class('resnetUnPooling.NyuDataset', M)
+
+function NyuDataset:__init(info, opt, split)
+    self.info = info[split]
+    self.opt = opt
+    self.split = split
+    self.dir = paths.concat(opt.data, split)
+    assert(paths.dirp(self.dir), 'directory does not exist: ' .. self.dir)
+end
+
+function NyuDataset:get(i)
+    local element = {}
+    local img, depth = self:__loadImageDepth(self.info.imagePath[i],
+    self.info.depthPath[i])
+    element.image = img
+    element.depth = depth
+    return element
+end
+
+function NyuDataset:__loadImageDepth(img, depth)
+    return image.loadJPG(img), torch.load(depth)
+end
+
+function NyuDataset:size()
+    return #self.info.imagePath
+end
+
+function NyuDataset.preprocess(opt, split)
+    -- transformation combination
+    local trans = T.Compose({
+        T.RandomScale(1, 1.5),
+        T.HorizontalFlip(0.5),
+        T.Rotation(5),
+        T.Color(0.8, 1,2),
+        T.RandomCrop(228, 304, 128, 160)
+    })
+    -- from the original dataset, generate val and train set
+    if split == 'test' then
+        G.augmentation(opt.testImage, opt.testDepth, opt,
+        split, opt.trainDataPortion, trans)
+    else
+        G.augmentation(opt.imageOrigin, opt.depthOrigin, opt,
+        split, opt.trainDataPortion, trans)
+    end
+end
+
+function NyuDataset.preprocessOnline()
+    local trans = T.Compose({
+        T.RandomScale(1, 1.5),
+        T.HorizontalFlip(0.5),
+        T.Rotation(5),
+        T.Color(0.8, 1,2),
+        T.RandomCrop(304, 228, 160, 128)
+    })
+
+    return G.augOneMatch(trans)
+end
+
+function NyuDataset.info(opt, cache)
+    return G.exec(opt, cache)
+end
+
+return M.NyuDataset
